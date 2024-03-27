ef747e2629a3dc60f1abd65d87f79af0395b8f50
Cinda <cindasibichan2002@gmail.com>
2024-03-27 14:21:39 +0530

    input field is added

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from . models import Products
+from . models import Products,Musician,Album
 
 # Register your models here.
 admin.site.register(Products)
+admin.site.register(Musician)
+admin.site.register(Album)
--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 # Create your models here.
 class Products(models.Model):
+        
+    
     image = models.ImageField(null=False,blank=False)
     name = models.CharField(max_length=200,null =False,blank=False)
     price = models.DecimalField(null =False,blank = False,max_digits=5,decimal_places =2)
@@ -10,6 +12,27 @@ class Products(models.Model):
     is_published = models.BooleanField(default = True)
     created_dat = models.DateTimeField(default = datetime.now,blank = False)
 
+    class Meta:
+        db_table = "Product_table"
+
     def __str__(self) -> str:
         return self.name
+    
+class  Musician(models.Model):
+    f_name = models.CharField(max_length=50,null=False,blank = False)
+    l_name = models.CharField(max_length=50,null = False,blank = False)
+    instrument = models.CharField(max_length=100,null = False,blank = False)
+
+    def __str__(self) -> str:
+        return self.f_name
+    
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician,on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    released_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
+
 
